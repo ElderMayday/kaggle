b1e092dff89da4181131972b43d97a3465f8ca0e
ElderMayday <galdarsar@gmail.com>
2017-04-30 22:53:32 +0200

    Redo feature selection as function

--- model.R
+++ model.R
@@ -1,12 +1,21 @@
 train_raw = read.csv("D:\\kaggle\\train.csv", header = TRUE)
 
 
-#PoolQC is however excluded since in is uneffective (sparse actual values - the information gain could be random)
+#Filters the train dataframe
 #Only higher than 0.1 IG features were selected
+#PoolQC is however excluded since in is uneffective (sparse actual values - the information gain could be random)
 
-train = subset(train_raw, select=c(OverallQual, Neighborhood, Alley, GrLivArea, 
-GarageCars, GarageArea, YearBuilt, ExterQual, KitchenQual, TotalBsmtSF, BsmtQual, MSSubClass,
-X1stFlrSF, FullBath, GarageYrBlt, GarageFinish, FireplaceQu, YearRemodAdd, Foundation, Fence,
-LotFrontage, GarageType, TotRmsAbvGrd, X2ndFlrSF, MiscFeature, Fireplaces, LotArea, OpenPorchSF,
-Exterior1st, Exterior2nd, HeatingQC, BsmtFinType1, BsmtFinSF1, MSZoning, MasVnrArea, MasVnrType,
-HouseStyle, OverallCond, WoodDeckSF, HalfBath, BsmtUnfSF, SalePrice))
+feature_filter <- function(input) {
+  result <- subset(input, select=c(OverallQual, Neighborhood, Alley, GrLivArea, 
+                                       GarageCars, GarageArea, YearBuilt, ExterQual, KitchenQual, TotalBsmtSF, BsmtQual, MSSubClass,
+                                       X1stFlrSF, FullBath, GarageYrBlt, GarageFinish, FireplaceQu, YearRemodAdd, Foundation, Fence,
+                                       LotFrontage, GarageType, TotRmsAbvGrd, X2ndFlrSF, MiscFeature, Fireplaces, LotArea, OpenPorchSF,
+                                       Exterior1st, Exterior2nd, HeatingQC, BsmtFinType1, BsmtFinSF1, MSZoning, MasVnrArea, MasVnrType,
+                                       HouseStyle, OverallCond, WoodDeckSF, HalfBath, BsmtUnfSF, SalePrice))
+  return(result)
+}
+
+
+
+
+train = feature_filter(train_raw)
