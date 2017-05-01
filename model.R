8f68ac3ad3644c4e527efd849229c1e9f3dd03c9
ElderMayday <galdarsar@gmail.com>
2017-05-01 05:54:50 +0200

    Start tree model

--- model.R
+++ model.R
@@ -1,21 +1,28 @@
-train_raw = read.csv("D:\\kaggle\\train.csv", header = TRUE)
-
+library(tree)
 
 #Filters the train dataframe
 #Only higher than 0.1 IG features were selected
-#PoolQC is however excluded since in is uneffective (sparse actual values - the information gain could be random)
+#PoolQC, Fence and MiscFeature are however excluded since in is uneffective (sparse actual values - the information gain could be random)
 
 feature_filter <- function(input) {
   result <- subset(input, select=c(OverallQual, Neighborhood, Alley, GrLivArea, 
-                                       GarageCars, GarageArea, YearBuilt, ExterQual, KitchenQual, TotalBsmtSF, BsmtQual, MSSubClass,
-                                       X1stFlrSF, FullBath, GarageYrBlt, GarageFinish, FireplaceQu, YearRemodAdd, Foundation, Fence,
-                                       LotFrontage, GarageType, TotRmsAbvGrd, X2ndFlrSF, MiscFeature, Fireplaces, LotArea, OpenPorchSF,
-                                       Exterior1st, Exterior2nd, HeatingQC, BsmtFinType1, BsmtFinSF1, MSZoning, MasVnrArea, MasVnrType,
-                                       HouseStyle, OverallCond, WoodDeckSF, HalfBath, BsmtUnfSF, SalePrice))
+                                   GarageCars, GarageArea, YearBuilt, ExterQual, KitchenQual, TotalBsmtSF, BsmtQual, MSSubClass,
+                                   X1stFlrSF, FullBath, GarageYrBlt, GarageFinish, FireplaceQu, YearRemodAdd, Foundation,
+                                   LotFrontage, GarageType, TotRmsAbvGrd, X2ndFlrSF, Fireplaces, LotArea, OpenPorchSF,
+                                   Exterior1st, Exterior2nd, HeatingQC, BsmtFinType1, BsmtFinSF1, MSZoning, MasVnrArea, MasVnrType,
+                                   HouseStyle, OverallCond, WoodDeckSF, HalfBath, BsmtUnfSF, SalePrice))
   return(result)
 }
 
-
-
-
+train_raw = read.csv("D:\\kaggle\\train.csv", header = TRUE)
 train = feature_filter(train_raw)
+regtree = tree(SalePrice~., train) 
+plot(regtree)
+#text(regtree)
+
+
+
+row = train[,1:(ncol(train)-1)]
+row[,"PredictedPrice"] = predict(regtree, row)
+row[,"RealPrice"] = train[,"SalePrice"]
+
